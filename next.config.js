b74241a5eb72f226045e61374e2aac2456475384
PhiLV <phi@cck.co.jp>
2023-12-12 17:18:25 +0700

    update secret google

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     GOOGLE_CLIENT_ID:
       '96305492305-bout7jov9hgpl0uc9itmv8b7t86sc4ua.apps.googleusercontent.com',
-    GOOGLE_CLIENT_SECRET: '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',
+    GOOGLE_CLIENT_SECRET: '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',
     NEXTAUTH_URL: 'http://localhost:3000/',
     NEXTAUTH_URL_INTERNAL: 'http://localhost:3000/',
     NEXTAUTH_SECRET: 'c36cefe4d56653b1c615f4e0fbe580e3',
