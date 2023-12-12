cb8b1565fb5ed5965ce76ff0a247e953f2fb3503
VinhNguyen <vinh@cck.co.jp>
2023-12-12 17:01:10 +0700

    [Update] Fix login with google

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    GOOGLE_CLIENT_ID:
+      '96305492305-bout7jov9hgpl0uc9itmv8b7t86sc4ua.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: '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',
+    NEXTAUTH_URL: 'http://localhost:3000/',
+    NEXTAUTH_URL_INTERNAL: 'http://localhost:3000/',
+    NEXTAUTH_SECRET: 'c36cefe4d56653b1c615f4e0fbe580e3',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
