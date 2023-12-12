6f62d72d4f8e30dafe6256839412cd2c1f2c1f1b
Hoang Nguyen <hoang@cck.co.jp>
2023-12-12 16:29:02 +0700

    [fix] config axios

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const GOOGLE_CLIENT_ID = "96305492305-bout7jov9hgpl0uc9itmv8b7t86sc4ua.apps.googleusercontent.com"
+export const GOOGLE_CLIENT_SECRET = "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"
+export const NEXTAUTH_URL = "http://localhost:3000"
+export const NEXT_PUBLIC_API_URL = "https://sheets.googleapis.com/v4/spreadsheets"
---
+++ src/utils/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+// Create an instance of Axios with custom configuration
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Set your API base URL in environment variables
+  timeout: 10000, // Set a timeout for requests
+  headers: {
+    'Content-Type': 'application/json',
+    // You can add any other headers here
+  },
+});
+
+export default api;
