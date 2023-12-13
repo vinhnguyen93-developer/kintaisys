8837b1706f8551445df62d1b772869f2f8c5e038
PhiLV <phi@cck.co.jp>
2023-12-13 23:03:05 +0700

    update scope read, write google sheet

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,6 +11,7 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: { params: { scope: "openid email profile https://www.googleapis.com/auth/spreadsheets" } },
     }),
   ],
   callbacks: {
