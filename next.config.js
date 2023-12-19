154a6427ce31dbfe53e03d41edfc9e67472ac474
VinhNguyen <vinh@cck.co.jp>
2023-12-19 08:43:47 +0700

    [Update]

--- next.config.js
+++ next.config.js
@@ -3,15 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     GOOGLE_CLIENT_ID:
-      '96305492305-bout7jov9hgpl0uc9itmv8b7t86sc4ua.apps.googleusercontent.com',
-    GOOGLE_CLIENT_SECRET: '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',
-    NEXTAUTH_URL: 'http://localhost:3000/',
-    NEXTAUTH_URL_INTERNAL: 'http://localhost:3000/',
+      '96305492305-fo21k488mu622784qs2p9ssrcid86vih.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: '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',
+    NEXTAUTH_URL: 'https://dxzjrjh55b9b.cloudfront.net',
+    NEXTAUTH_URL_INTERNAL: 'https://dxzjrjh55b9b.cloudfront.net',
     NEXTAUTH_SECRET: 'c36cefe4d56653b1c615f4e0fbe580e3',
     NEXT_PUBLIC_API_URL: 'https://sheets.googleapis.com/v4/spreadsheets',
     SHEET_ID: '12KAc4XeZcQ-mUTgU37Hu0fVEXPyl7zKTmn5NmHWq2Tk',
     API_KEY: 'AIzaSyCKVNGMGP904d8nCyPKPDvVfK-DekXlAhc',
   },
+  output: 'export',
 };
 
 module.exports = nextConfig;
