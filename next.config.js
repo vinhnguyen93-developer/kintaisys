771dceebbf082d3b19e04e8a39b77a818c5cd23b
VinhNguyen <vinh@cck.co.jp>
2023-12-19 09:06:13 +0700

    [Update] domain

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     GOOGLE_CLIENT_ID:
       '96305492305-fo21k488mu622784qs2p9ssrcid86vih.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET: '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',
-    NEXTAUTH_URL: 'https://dxzjrjh55b9b.cloudfront.net',
-    NEXTAUTH_URL_INTERNAL: 'https://dxzjrjh55b9b.cloudfront.net',
+    NEXTAUTH_URL: 'https://kintaisys.vercel.app/',
+    NEXTAUTH_URL_INTERNAL: 'https://kintaisys.vercel.app/',
     NEXTAUTH_SECRET: 'c36cefe4d56653b1c615f4e0fbe580e3',
     NEXT_PUBLIC_API_URL: 'https://sheets.googleapis.com/v4/spreadsheets',
     SHEET_ID: '12KAc4XeZcQ-mUTgU37Hu0fVEXPyl7zKTmn5NmHWq2Tk',
