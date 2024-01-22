16690b9ca604f89d23e00b3ae04ace6d4d0a4a42
Hoang Nguyen <hoangquoc197@gmail.com>
2024-01-22 16:13:07 +0700

    [fix] domain nexxtauth

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     GOOGLE_CLIENT_ID:
       '96305492305-6hb1a4r04oagd66ttrgtl4fjnrs3383n.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET: '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',
-    NEXTAUTH_URL: 'http://localhost:3000/',
-    NEXTAUTH_URL_INTERNAL: 'http://localhost:3000/',
+    NEXTAUTH_URL: 'https://kintaisys.vercel.app/',
+    NEXTAUTH_URL_INTERNAL: 'https://kintaisys.vercel.app/',
     NEXTAUTH_SECRET: 'c36cefe4d56653b1c615f4e0fbe580e3',
     NEXT_PUBLIC_API_URL: 'https://sheets.googleapis.com/v4/spreadsheets',
     SHEET_ID: '1QMqKfjVt4U_so4LyltUQaXbyOwupQuppIqkZ7hYH2yM',
